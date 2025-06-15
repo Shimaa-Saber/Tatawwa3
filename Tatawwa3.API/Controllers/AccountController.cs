7414700af276bb9b0f6df244fdff0688328f7850
Shimaa-Saber <shimaasaber812@gmail.com>
2025-06-15 13:47:07 +0300

    add class

---
+++ Tatawwa3.API/Controllers/AccountController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Tatawwa3.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AccountController : ControllerBase
+    {
+    }
+}
