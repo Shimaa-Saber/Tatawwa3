4e64c53fc073bdb1c53c0d9ae8994f93d659582c
Shimaa-Saber <shimaasaber812@gmail.com>
2025-06-25 23:46:05 +0300

    Add general response

---
+++ Tatawwa3.Domain/Enums/ErrorCode.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tatawwa3.Domain.Enums
+{
+    public enum ErrorCode
+    {
+        None = 0,
+        ValidationError = 1,
+        UnExcepectedError = 2,
+        CategoryNotFound = 100,
+        CategoryAlreadyExist = 101,
+    }
+}
