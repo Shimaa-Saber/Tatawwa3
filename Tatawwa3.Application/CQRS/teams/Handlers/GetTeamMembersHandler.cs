96ef25aabe043542b855823ea0e5da8a6487d76b
Christena <krisayman13@gmail.com>
2025-07-19 02:35:30 +0300

    update

--- Tatawwa3.Application/CQRS/teams/Handlers/GetTeamMembersHandler.cs
+++ Tatawwa3.Application/CQRS/teams/Handlers/GetTeamMembersHandler.cs
@@ -21,10 +21,25 @@ namespace Tatawwa3.Application.CQRS.teams.Handlers
             _context = context;
         }
 
+        //public async Task<List<TeamMemberDto>> Handle(GetTeamMembersQuery request, CancellationToken cancellationToken)
+        //{
+        //    return await _context.TeamMembers
+        //        .Where(tm => tm.TeamID == request.TeamId && tm.Status == TeamMemberStatus.Accepted)
+        //        .Include(tm => tm.Volunteer)
+        //            .ThenInclude(v => v.User)
+        //        .Select(tm => new TeamMemberDto
+        //        {
+        //            VolunteerId = tm.VolunteerID,
+        //            Name = tm.Volunteer.User != null ? tm.Volunteer.User.UserName : "Unknown",
+        //            ProfileImageUrl = tm.Volunteer.ProfilePictureUrl ?? "",
+        //            Role = tm.Role ?? ""
+        //        })
+        //        .ToListAsync();
+        //}
         public async Task<List<TeamMemberDto>> Handle(GetTeamMembersQuery request, CancellationToken cancellationToken)
         {
             return await _context.TeamMembers
-                .Where(tm => tm.TeamID == request.TeamId && tm.Status == TeamMemberStatus.Accepted)
+                .Where(tm => tm.TeamID == request.TeamId) // <-- شرط Accepted اتشال
                 .Include(tm => tm.Volunteer)
                     .ThenInclude(v => v.User)
                 .Select(tm => new TeamMemberDto
@@ -36,5 +51,6 @@ namespace Tatawwa3.Application.CQRS.teams.Handlers
                 })
                 .ToListAsync();
         }
+
     }
 }
